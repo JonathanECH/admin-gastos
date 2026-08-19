9c531c8e01b3a2570fed2a217e77cdac5643e21d
JonathanECH <jonathanecampos3@gmail.com>
2026-08-19 16:09:51 -0400

    Se creó el botón para agregar un nuevo gasto.

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,7 @@
 import { ref } from 'vue';
 import Presupuesto from './components/Presupuesto.vue';
 import ControlPresupuesto from './components/ControlPresupuesto.vue';
+import iconoNuevoGasto from './assets/img/nuevo-gasto.svg'
 const presupuesto = ref(0);
 const disponible = ref(0);
 const definirPresupuesto = cantidad => {
@@ -15,13 +16,16 @@ const definirPresupuesto = cantidad => {
     <header>
       <h1>Planificador De Gastos</h1>
       <div class="contenedor-header contenedor sombra">
-        <Presupuesto v-if="presupuesto === 0"
-        @definir-presupuesto="definirPresupuesto"/>
-        <ControlPresupuesto v-else 
-        :presupuesto="presupuesto"
-        :disponible="disponible"/>
+        <Presupuesto v-if="presupuesto === 0" @definir-presupuesto="definirPresupuesto" />
+        <ControlPresupuesto v-else :presupuesto="presupuesto" :disponible="disponible" />
       </div>
     </header>
+    <main v-if="presupuesto > 0">
+
+      <div class="gasto-nuevo">
+        <img :src="iconoNuevoGasto" alt="Icono nuevo gasto">
+      </div>
+    </main>
   </div>
 </template>
 
@@ -77,5 +81,24 @@ header {
   background-color: $blanco;
   border-radius: 1.2rem;
   padding: 5rem;
+}
+
+.gasto-nuevo {
+  position: fixed;
+  right: 2.5rem;
+  bottom: 2.5rem;
+
+  @include m.tablet {
+    right: 5rem;
+    bottom: 5rem;
+  }
+
+  img {
+    width: 5rem;
+
+    &:hover {
+      cursor: pointer;
+    }
+  }
 }
 </style>
